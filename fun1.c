74011f92156a32c026b7e7655d3ae2bca305ffa1
Julia <julie.tataurova@yandex.ru>
2023-08-20 01:03:46 +0300

    getchar instead of scanf

--- fun1.c
+++ fun1.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <ctype.h>
+#include <stdlib.h>
+#include <string.h>
+#include <float.h>
 #include <math.h>
+
 int comparison(double x)
 {
     long double compare_number = 1e-9;
@@ -33,14 +37,48 @@ int solve(double a, double b, double c, double *answer1, double *answer2, int *n
 
 int main()
 {
-    double a, b, c;
-    int check;
-    printf("enter the coefficients\n");
-    while ((check = scanf("%lf%lf%lf", &a, &b, &c)) != 3)
+    printf("enter the coefficients separated by spaces:\n");
+    double coefficients[3];
+    char x, number[DBL_DIG];
+    int number_of_coefficients = 0;
+    int i = 0;
+    while ((x = getchar()) && number_of_coefficients < 3)
     {
-        printf("it is not a digit! please, try again.\n");
-        fflush(stdin);
+        if (isdigit(x))
+        {
+            number[i] = x;
+            i++;
+        }
+        else if ((isspace(x) || x == '\n') && isdigit(number[i - 1]))
+        {
+            number[i] = '\0';
+            i = 0;
+            coefficients[number_of_coefficients] = atof(number);
+            memset(number, '0', 11);
+            number_of_coefficients++;
+            if (x == '\n')
+            {
+                break;
+            }
+        }
+        else if (x == '-' && i == 0)
+        {
+            number[i] = x;
+            i++;
+        }
+        else if (x == '.' && isdigit(number[i - 1]))
+        {
+            number[i] = x;
+            i++;
+        }
+        else
+        {
+            i = 0;
+            memset(number, '0', DBL_DIG);
+        }
     }
+    double a = coefficients[0], b = coefficients[1], c = coefficients[2];
+    printf("%lf %lf %lf\n", a, b, c);
     if (comparison((b * b - 4 * a * c)))
     {
         printf("no roots\n");
