980e60fd8560b0ad85f7d0add7146c4b7375fdd4
Julia <julie.tataurova@yandex.ru>
2023-08-19 23:42:07 +0300

    no malloc; new function called compare_number

--- fun1.c
+++ fun1.c
@@ -1,58 +1,56 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <ctype.h>
 #include <math.h>
+int comparison(double x)
+{
+    long double compare_number = 1e-9;
+    return (compare_number - x) > 0 ? 1 : 0;
+}
 
-void solve(double a, double b, double c, double *answer, int *n)
+int solve(double a, double b, double c, double *answer1, double *answer2, int *n)
 {
     long double compare_number = 1e-9;
     double d = b * b - 4 * a * c;
-    if (d < compare_number)
+    if (comparison(fabs(a)) && !comparison(fabs(b)))
     {
-        answer = (double *)realloc(answer, sizeof(double) * 1);
         *n = 1;
-        answer[0] = ((-1) * b / (2 * a));
+        *answer1 = (-1) * c / b;
+    }
+    else if (comparison(d))
+    {
+        *n = 1;
+        *answer1 = ((-1) * b / (2 * a));
     }
     else
     {
         *n = 2;
         double sq_d = sqrt(d);
-        answer[0] = ((-1) * b + sq_d) / (2 * a);
-        answer[1] = ((-1) * b - sq_d) / (2 * a);
+        *answer1 = ((-1) * b + sq_d) / (2 * a);
+        *answer2 = ((-1) * b - sq_d) / (2 * a);
     }
+    return 0;
 }
 
 int main()
 {
-    long double compare_number = 1e-9;
     double a, b, c;
-    int check, n;
+    int check;
     printf("enter the coefficients\n");
     while ((check = scanf("%lf%lf%lf", &a, &b, &c)) != 3)
     {
         printf("it is not a digit! please, try again.\n");
         fflush(stdin);
     }
-    if (fabs(a) < compare_number)
+    if (comparison((b * b - 4 * a * c)))
     {
-        printf("it's not a quadratic equation\n");
+        printf("no roots\n");
     }
     else
     {
-        if ((b * b - 4 * a * c) < compare_number)
-        {
-            printf("no roots\n");
-        }
-        else
-        {
-            printf("the roots are:\n");
-            double *answer = (double *)malloc(sizeof(double) * 2);
-            solve(a, b, c, answer, &n);
-            for (int i = 0; i < n; i++)
-            {
-                printf("x%d = %lf\n", i + 1, answer[i]);
-            }
-            free(answer);
-        }
+        double answer1, answer2;
+        int n;
+        solve(a, b, c, &answer1, &answer2, &n);
+        n == 2 ? printf("the roots are:\nx1 = %lf\nx2 = %lf", answer1, answer2) : printf("the root is:\nx = %lf\n", answer1);
     }
     return 0;
 }
