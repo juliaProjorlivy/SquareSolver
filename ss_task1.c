7b745acbb481d0ca945c68078ea5b01441765e94
Julia <julie.tataurova@yandex.ru>
2023-08-22 15:49:18 +0300

    no more getchar, istead scanf + clear_buf; enum

--- ss_task1.c
+++ ss_task1.c
@@ -6,16 +6,26 @@
 #include <math.h>
 #include <assert.h>
 
+enum NUMBER_OF_SOLUTIONS
+{
+    NO_ROOTS = 0,
+    ONE_ROOT = 1,
+    TWO_ROOTS = 2,
+    INFINITY_SOL = 3
+};
+
 int is_equal_zero(double x)
 {
     const double EPSILON = 1e-9;
-    return (EPSILON > fabs(x));
+    return (fabs(x) < EPSILON);
 }
 
 int solve_linear(double b, double c, double *answer1, int number_of_roots)
 {
-    number_of_roots = is_equal_zero(b) ? 3 : 1;
-    *answer1 = is_equal_zero(b) ? 0 : ((-1) * c / b);
+    number_of_roots = is_equal_zero(b) ? INFINITY_SOL : ONE_ROOT;
+    double value = is_equal_zero(((-1) * c / b)) ? 0 : ((-1) * c / b);
+    *answer1 = is_equal_zero(b) ? 0 : value;
+
     return number_of_roots;
 }
 
@@ -25,96 +35,118 @@ int solve_quadratic_equation(double a, double b, double c, double *answer1, doub
     double d = b * b - 4 * a * c;
     if (d < 0)
     {
-        number_of_roots = 0;
+        return 0;
     }
-    else if (is_equal_zero(a))
+    if (is_equal_zero(a))
     {
-        number_of_roots = solve_linear(b, c, answer1, number_of_roots);
+        return (solve_linear(b, c, answer1, number_of_roots));
     }
-    else if (is_equal_zero(d))
+    if (is_equal_zero(d))
     {
-        number_of_roots = 1;
         *answer1 = ((-1) * b / (2 * a));
+        return 1;
     }
     else
     {
-        number_of_roots = 2;
         double sq_d = sqrt(d);
         *answer1 = ((-1) * b + sq_d) / (2 * a);
         *answer2 = ((-1) * b - sq_d) / (2 * a);
+        return 2;
     }
-    return number_of_roots;
+}
+
+void clear_buf()
+{
+    while (getchar() != '\n')
+        ;
 }
 
 int get_coefficients(double *a, double *b, double *c)
 {
-    double coefficients[3] = {0, 0, 0};
-    char number_buf[DBL_DIG] = {'0'};
-    char symbol = '0';
-    int number_of_arg = 0;
-    int index = 0, nothing_before_digit = 1;
-    while (((symbol = getchar()) != EOF) && (number_of_arg < 3))
-    {
-        if (isdigit(symbol) && nothing_before_digit)
-        {
-            number_buf[index] = symbol;
-            index++;
-        }
-        else if ((isspace(symbol)))
-        {
-            nothing_before_digit = 1;
-            if (isdigit(number_buf[index - 1]) || isspace(number_buf[index - 1]))
-            {
-                number_buf[index] = '\0';
-                index = 0;
-                coefficients[number_of_arg] = atof(number_buf);
-                memset(number_buf, '0', DBL_DIG);
-                number_of_arg += 1;
-            }
-            if (symbol == '\n')
-            {
-                if (number_of_arg < 3)
-                {
-                    printf("the number of arguments is less than 3. please, try again:\n");
-                    number_of_arg = 0;
-                    index = 0;
-                    memset(number_buf, '0', DBL_DIG);
-                }
-                else
-                {
-                    break;
-                }
-            }
+    int check = scanf("%lf%lf%lf", a, b, c);
 
-            else
-            {
-                index = 0;
-                memset(number_buf, '0', DBL_DIG);
-            }
-        }
-        else if (symbol == '-' && index == 0)
-        {
-            number_buf[index] = symbol;
-            index++;
-        }
-        else if (symbol == '.' && isdigit(number_buf[index - 1]))
-        {
-            number_buf[index] = symbol;
-            index++;
-        }
-        else
-        {
-            nothing_before_digit = 0;
-            index = 0;
-            memset(number_buf, '0', DBL_DIG);
-        }
+    while (check != 3)
+    {
+        clear_buf();
+        printf("it is not a digit! please, try again.\n");
+        check = scanf("%lf%lf%lf", a, b, c);
     }
-    *a = coefficients[0], *b = coefficients[1], *c = coefficients[2];
-    assert(a != NULL);
-    assert(b != NULL);
-    assert(c != NULL);
-    return number_of_arg;
+
+    return check;
 }
+
+// int get_coefficients(double *a, double *b, double *c)
+// {
+//     assert(a != NULL);
+//     assert(b != NULL);
+//     assert(c != NULL);
+
+//     double coefficients[3] = {0, 0, 0};
+//     char number_buf[DBL_DIG] = {'\0'};
+//     char symbol = '0';
+//     int number_of_arg = 0;
+//     int index = 0, nothing_before_digit = 1;
+//     // while(getchar() != '\n')
+//     // scanf
+//     while (((symbol = getchar()) != EOF) && (number_of_arg < 3))
+//     {
+//         if (isdigit(symbol) && nothing_before_digit)
+//         {
+//             number_buf[index] = symbol;
+//             index++;
+//         }
+//         else if ((isspace(symbol)))
+//         {
+//             nothing_before_digit = 1;
+//             if (isdigit(number_buf[index - 1]) || isspace(number_buf[index - 1]))
+//             {
+//                 number_buf[index] = '\0';
+//                 index = 0;
+//                 coefficients[number_of_arg] = atof(number_buf);
+//                 memset(number_buf, '0', DBL_DIG);
+//                 number_of_arg += 1;
+//             }
+//             if (symbol == '\n')
+//             {
+//                 if (number_of_arg < 3)
+//                 {
+//                     printf("the number of arguments is less than 3. please, try again:\n");
+//                     number_of_arg = 0;
+//                     index = 0;
+//                     memset(number_buf, '0', DBL_DIG);
+//                 }
+//                 else
+//                 {
+//                     break;
+//                 }
+//             }
+
+//             else
+//             {
+//                 index = 0;
+//                 memset(number_buf, '0', DBL_DIG);
+//             }
+//         }
+//         else if (symbol == '-' && index == 0)
+//         {
+//             number_buf[index] = symbol;
+//             index++;
+//         }
+//         else if (symbol == '.' && isdigit(number_buf[index - 1]))
+//         {
+//             number_buf[index] = symbol;
+//             index++;
+//         }
+//         else
+//         {
+//             nothing_before_digit = 0;
+//             index = 0;
+//             memset(number_buf, '0', DBL_DIG);
+//         }
+//     }
+//     *a = coefficients[0], *b = coefficients[1], *c = coefficients[2];
+//     return number_of_arg;
+// }
 
 int main()
 {
@@ -131,16 +163,16 @@ int main()
         assert(&answer2 != NULL);
         switch (number_of_roots)
         {
-        case 0:
+        case NO_ROOTS:
             printf("no roots\n");
             break;
-        case 1:
+        case ONE_ROOT:
             printf("the root is:\nx = %lf\n", answer1);
             break;
-        case 2:
+        case TWO_ROOTS:
             printf("the roots are:\nx1 = %lf\nx2 = %lf\n", answer1, answer2);
             break;
-        case 3:
+        case INFINITY_SOL:
             printf("infinite number of roots\n");
             break;
         }
