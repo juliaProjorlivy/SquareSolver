361b86cb3b1cda0af550f913267ebc9746008711
julia <julie.tataurova@yandex.ru>
2023-08-23 21:02:54 +0300

    added struct for tests

--- full_task/test.cpp
+++ full_task/test.cpp
@@ -5,6 +5,15 @@
 #include <stdlib.h>
 #include <math.h>
 
+#define GREEN "\033[92m"
+#define END "\n\x1b[39;49m"
+#define RED "\033[91m"
+
+double precision(double x, double y){
+    const double EPSILON = 1e-9;
+    return fabs(x - y) < EPSILON;
+}
+
 double rounding(double x, int n){
     double power_ten1 = pow(10, (n+1));
     double power_ten2 = pow(10, (n));
@@ -24,37 +33,34 @@ typedef struct test test;
 void test_square(test *data){
     double answer1 = 0, answer2 = 0;
     int nRoots = solve_quadratic_equation((data->abc)[0], (data->abc)[1], (data->abc)[2], &answer1, &answer2);
-    printf("a = %lf\n", (data->abc)[0]);
-    // answer1 = rounding(answer1, 5);
-    // answer2 = rounding(answer2, 5);
+    answer1 = rounding(answer1, 5);
+    answer2 = rounding(answer2, 5);
 
-    if(answer1!=((data->answersRef)[0]) || answer2!=((data->answersRef)[1]) || nRoots!=(data->nRootsRef))
+
+    if(!precision(answer1, ((data->answersRef)[0])) || !precision(answer2, ((data->answersRef)[1])) || nRoots!=(data->nRootsRef))
     {
-        printf("test case failed:\nx1 = %lf; x2 = %lf; nRoots = %d\n"
+        printf(RED "test case failed:" END);
+        printf("x1 = %lf; x2 = %lf; nRoots = %d\n"
         "expected:\nx1 = %lf; x2 = %lf; nRoots = %d\n ", answer1, answer2,
         nRoots, (data->answersRef)[0], (data->answersRef)[1], data->nRootsRef);
     }
     else
     {
-        printf("test case passed successully\n");
+        printf(GREEN "test case passed successully" END);
     }
 }
 
 
 void run_tests()
 {
-//{.abc = {}, .answersRef = {}, .nRootsRef = 0 };
-    struct test tests[] = {.abc = {}, .answersRef = {}, .nRootsRef = 0 };
-    struct test *tests = &tests;
-    testss->abc = {5, 10, -15};
-    testss->answersRef = {1, -1};
-    testss->nRootsRef = 2;
-    test_square(testss);
-    // size_t length = sizeof(tests)/sizeof(tests[0]);
-    // for(int index = 1; index <= (int)length; index++){
-    //     printf("%d ", index);
-    //     test_square(tests);
-    // }
+    test tests[] = {{{5, 10, -15}, {1, -3}, 2}, 
+                    {{0, 0, 0}, {0, 0}, 3}, 
+                    {{1.3, 1.54, -15}, {2.85578, -4.04039}, 2}, 
+                    {{0, 1, 0}, {0, 0}, 1}, 
+                    {{1.9, -2.1, -6}, {2.41362, -1.30836}, 2}};
+    size_t length = sizeof(tests)/sizeof(tests[0]);
+    for(int index = 0; index < (int)length; index++){
+        printf("%d ", index + 1);
+        test_square(&tests[index]);
+    }
 }
-//  {{1.3, 1.54, -15}, {2.85578, -4.04039}, 2},
-//{{0, 0, 0}, {0, 0}, 3}
